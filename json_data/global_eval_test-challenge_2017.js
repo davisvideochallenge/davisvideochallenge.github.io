80842dc00cdd8810bf0ac4ba31af8bd2e3e90f75
Jordi Pont-Tuset <jponttuset@gmail.com>
2017-07-10 00:11:04 +0200

    - Test-challenge results on the web

---
+++ json_data/global_eval_test-challenge_2017.js
@@ -0,0 +1,273 @@
+
+var tech_props = {"lixx"          : {"type": "over", "display_name": "lixx", "sets": ['test-challenge-2017']},
+				  "apata"         : {"type": "over", "display_name": "apata", "sets": ['test-challenge-2017']},
+				  "vantam299"     : {"type": "over", "display_name": "vanta", "sets": ['test-challenge-2017']},
+				  "haamooon"      : {"type": "over", "display_name": "haamo", "sets": ['test-challenge-2017']},
+				  "voigtlaender"  : {"type": "over", "display_name": "voigt", "sets": ['test-challenge-2017']},
+				  "lalalafine123" : {"type": "over", "display_name": "lalal", "sets": ['test-challenge-2017']},
+				  "cjc"           : {"type": "over", "display_name": "cjc", "sets": ['test-challenge-2017']},
+				  "YXLKJ"         : {"type": "over", "display_name": "YXLKJ", "sets": ['test-challenge-2017']},
+				  "wasidennis"    : {"type": "over", "display_name": "wasid", "sets": ['test-challenge-2017']},
+				  "Fromandtozh"   : {"type": "over", "display_name": "Froma", "sets": ['test-challenge-2017']},
+				  "zwrq0"         : {"type": "over", "display_name": "zwrq0", "sets": ['test-challenge-2017']},
+				  "drbear"        : {"type": "over", "display_name": "drbea", "sets": ['test-challenge-2017']},
+				  "anewswan"      : {"type": "over", "display_name": "anews", "sets": ['test-challenge-2017']},
+				  "ilanv"         : {"type": "over", "display_name": "ilanv", "sets": ['test-challenge-2017']},
+				  "koh"           : {"type": "over", "display_name": "koh", "sets": ['test-challenge-2017']},
+				  "make"          : {"type": "over", "display_name": "make", "sets": ['test-challenge-2017']},
+				  "kozabu"        : {"type": "over", "display_name": "kozab", "sets": ['test-challenge-2017']},
+				  "xn8812"        : {"type": "over", "display_name": "xn881", "sets": ['test-challenge-2017']},
+				  "zpd"           : {"type": "over", "display_name": "zpd", "sets": ['test-challenge-2017']},
+				  "griffb"        : {"type": "over", "display_name": "griff", "sets": ['test-challenge-2017']},
+				  "nitinsingh"    : {"type": "over", "display_name": "nitin", "sets": ['test-challenge-2017']},
+				  "Team542"       : {"type": "over", "display_name": "Team5", "sets": ['test-challenge-2017']}
+                  };
+
+
+var global_eval_test_challenge_2017 ={
+	"Tgt": "0.198",
+	"techniques": [
+		"lixx",
+		"apata",
+		"vantam299",
+		"haamooon",
+		"voigtlaender",
+		"lalalafine123",
+		"cjc",
+		"YXLKJ",
+		"wasidennis",
+		"Fromandtozh",
+		"zwrq0",
+		"drbear",
+		"anewswan",
+		"ilanv",
+		"koh",
+		"make",
+		"kozabu",
+		"xn8812",
+		"zpd",
+		"griffb",
+		"nitinsingh",
+		"Team542"
+	],
+	"lixx": {
+		"global_mean": "0.699",
+		"Jmean": "0.679",
+		"Jrecall": "0.746",
+		"Jdecay": "0.225",
+		"Fmean": "0.719",
+		"Frecall": "0.791",
+		"Fdecay": "0.241",
+		"T": "0.260"
+	},
+	"apata": {
+		"global_mean": "0.678",
+		"Jmean": "0.651",
+		"Jrecall": "0.725",
+		"Jdecay": "0.277",
+		"Fmean": "0.706",
+		"Frecall": "0.798",
+		"Fdecay": "0.302",
+		"T": "0.371"
+	},
+	"vantam299": {
+		"global_mean": "0.638",
+		"Jmean": "0.615",
+		"Jrecall": "0.686",
+		"Jdecay": "0.171",
+		"Fmean": "0.662",
+		"Frecall": "0.790",
+		"Fdecay": "0.176",
+		"T": "0.416"
+	},
+	"haamooon": {
+		"global_mean": "0.615",
+		"Jmean": "0.598",
+		"Jrecall": "0.710",
+		"Jdecay": "0.219",
+		"Fmean": "0.632",
+		"Frecall": "0.746",
+		"Fdecay": "0.237",
+		"T": "0.543"
+	},
+	"voigtlaender": {
+		"global_mean": "0.577",
+		"Jmean": "0.548",
+		"Jrecall": "0.608",
+		"Jdecay": "0.310",
+		"Fmean": "0.605",
+		"Frecall": "0.672",
+		"Fdecay": "0.347",
+		"T": "0.513"
+	},
+	"lalalafine123": {
+		"global_mean": "0.569",
+		"Jmean": "0.548",
+		"Jrecall": "0.607",
+		"Jdecay": "0.344",
+		"Fmean": "0.591",
+		"Frecall": "0.667",
+		"Fdecay": "0.361",
+		"T": "0.417"
+	},
+	"cjc": {
+		"global_mean": "0.569",
+		"Jmean": "0.536",
+		"Jrecall": "0.595",
+		"Jdecay": "0.253",
+		"Fmean": "0.602",
+		"Frecall": "0.679",
+		"Fdecay": "0.276",
+		"T": "0.671"
+	},
+	"YXLKJ": {
+		"global_mean": "0.558",
+		"Jmean": "0.538",
+		"Jrecall": "0.601",
+		"Jdecay": "0.377",
+		"Fmean": "0.578",
+		"Frecall": "0.621",
+		"Fdecay": "0.429",
+		"T": "0.472"
+	},
+	"wasidennis": {
+		"global_mean": "0.548",
+		"Jmean": "0.516",
+		"Jrecall": "0.563",
+		"Jdecay": "0.268",
+		"Fmean": "0.579",
+		"Frecall": "0.648",
+		"Fdecay": "0.288",
+		"T": "0.722"
+	},
+	"Fromandtozh": {
+		"global_mean": "0.539",
+		"Jmean": "0.507",
+		"Jrecall": "0.549",
+		"Jdecay": "0.325",
+		"Fmean": "0.571",
+		"Frecall": "0.632",
+		"Fdecay": "0.337",
+		"T": "0.613"
+	},
+	"zwrq0": {
+		"global_mean": "0.536",
+		"Jmean": "0.505",
+		"Jrecall": "0.549",
+		"Jdecay": "0.280",
+		"Fmean": "0.567",
+		"Frecall": "0.635",
+		"Fdecay": "0.304",
+		"T": "0.741"
+	},
+	"drbear": {
+		"global_mean": "0.519",
+		"Jmean": "0.505",
+		"Jrecall": "0.564",
+		"Jdecay": "0.341",
+		"Fmean": "0.533",
+		"Frecall": "0.579",
+		"Fdecay": "0.395",
+		"T": "0.259"
+	},
+	"anewswan": {
+		"global_mean": "0.509",
+		"Jmean": "0.490",
+		"Jrecall": "0.551",
+		"Jdecay": "0.213",
+		"Fmean": "0.528",
+		"Frecall": "0.583",
+		"Fdecay": "0.237",
+		"T": "0.756"
+	},
+	"ilanv": {
+		"global_mean": "0.497",
+		"Jmean": "0.460",
+		"Jrecall": "0.493",
+		"Jdecay": "0.331",
+		"Fmean": "0.533",
+		"Frecall": "0.584",
+		"Fdecay": "0.364",
+		"T": "0.659"
+	},
+	"koh": {
+		"global_mean": "0.491",
+		"Jmean": "0.459",
+		"Jrecall": "0.502",
+		"Jdecay": "0.361",
+		"Fmean": "0.523",
+		"Frecall": "0.571",
+		"Fdecay": "0.392",
+		"T": "0.923"
+	},
+	"make": {
+		"global_mean": "0.480",
+		"Jmean": "0.463",
+		"Jrecall": "0.500",
+		"Jdecay": "0.402",
+		"Fmean": "0.497",
+		"Frecall": "0.528",
+		"Fdecay": "0.448",
+		"T": "0.227"
+	},
+	"kozabu": {
+		"global_mean": "0.478",
+		"Jmean": "0.439",
+		"Jrecall": "0.458",
+		"Jdecay": "0.330",
+		"Fmean": "0.516",
+		"Frecall": "0.560",
+		"Fdecay": "0.363",
+		"T": "0.691"
+	},
+	"xn8812": {
+		"global_mean": "0.476",
+		"Jmean": "0.478",
+		"Jrecall": "0.563",
+		"Jdecay": "0.167",
+		"Fmean": "0.473",
+		"Frecall": "0.530",
+		"Fdecay": "0.216",
+		"T": "0.445"
+	},
+	"zpd": {
+		"global_mean": "0.471",
+		"Jmean": "0.449",
+		"Jrecall": "0.480",
+		"Jdecay": "0.318",
+		"Fmean": "0.493",
+		"Frecall": "0.544",
+		"Fdecay": "0.362",
+		"T": "0.646"
+	},
+	"griffb": {
+		"global_mean": "0.420",
+		"Jmean": "0.406",
+		"Jrecall": "0.421",
+		"Jdecay": "0.374",
+		"Fmean": "0.433",
+		"Frecall": "0.432",
+		"Fdecay": "0.402",
+		"T": "0.453"
+	},
+	"nitinsingh": {
+		"global_mean": "0.256",
+		"Jmean": "0.249",
+		"Jrecall": "0.123",
+		"Jdecay": "0.131",
+		"Fmean": "0.263",
+		"Frecall": "0.091",
+		"Fdecay": "0.130",
+		"T": "0.807"
+	},
+	"Team542": {
+		"global_mean": "0.112",
+		"Jmean": "0.118",
+		"Jrecall": "0.073",
+		"Jdecay": "0.140",
+		"Fmean": "0.106",
+		"Frecall": "0.030",
+		"Fdecay": "0.126",
+		"T": "0.866"
+	}
+}
