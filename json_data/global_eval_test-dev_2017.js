e78317d6e0e5d12905880edd5f7664c9e77768bd
scaelles <scaelles@gmail.com>
2018-03-03 20:50:14 +0100

    First version of Davis 2017 files

---
+++ json_data/global_eval_test-dev_2017.js
@@ -0,0 +1,16 @@
+var global_eval_test_dev_2017 = {
+	"Tgt": "0.088",
+	"techniques": [
+		"osvos"
+	],
+	"osvos": {
+		"J_F": "0.798",
+		"Jmean": "0.798",
+		"Jrecall": "0.936",
+		"Jdecay": "0.149",
+		"Fmean": "0.806",
+		"Frecall": "0.926",
+		"Fdecay": "0.150",
+		"T": "0.378"
+	}
+}
---
+++ json_data/global_eval_val_2017.js
@@ -0,0 +1,15 @@
+var global_eval_val_2017 = {
+	"Tgt": "0.088",
+	"techniques": [
+		"osvos"
+	],
+	"osvos": {
+		"Jmean": "0.798",
+		"Jrecall": "0.936",
+		"Jdecay": "0.149",
+		"Fmean": "0.806",
+		"Frecall": "0.926",
+		"Fdecay": "0.150",
+		"T": "0.378"
+	}
+}
