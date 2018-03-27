7566b0cbccbbfef7574526bd09c50480eb8b7c3f
scaelles <scaelles@gmail.com>
2018-03-27 16:26:06 +0200

    Email changed to davisvideochallenge

--- json_data/global_eval_test-dev_2017.js
+++ json_data/global_eval_test-dev_2017.js
@@ -1,15 +1,25 @@
 var global_eval_test_dev_2017 = {
 	"Tgt": "0.088",
 	"techniques": [
-		"osvos"
+		"osvos",
+		"onavos"
 	],
 	"osvos": {
- 		"J_F": "0.4865",
- 		"Jmean": "0.4463",
- 		"Jrecall": "0.4816",
- 		"Jdecay": "0.2132",
- 		"Fmean": "0.5267",
- 		"Frecall": "0.5569",
- 		"Fdecay": "0.2049"
+ 		"J_F": "0.5092",
+ 		"Jmean": "0.4704",
+ 		"Jrecall": "0.5215",
+ 		"Jdecay": "0.1916",
+ 		"Fmean": "0.5480",
+ 		"Frecall": "0.5970",
+ 		"Fdecay": "0.1976"
+ 	},
+    "onavos": {
+ 		"J_F": "0.5282",
+ 		"Jmean": "0.4991",
+ 		"Jrecall": "0.5426",
+ 		"Jdecay": "0.2299",
+ 		"Fmean": "0.5574",
+ 		"Frecall": "0.6028",
+ 		"Fdecay": "0.2339"
  	}
-}
+};
--- json_data/global_eval_val_2017.js
+++ json_data/global_eval_val_2017.js
@@ -1,15 +1,25 @@
 var global_eval_val_2017 = {
 	"Tgt": "0.088",
 	"techniques": [
-		"osvos"
+		"osvos",
+		"onavos"
 	],
 	"osvos": {
- 		"J_F": "0.5757",
- 		"Jmean": "0.5467",
- 		"Jrecall": "0.5968",
- 		"Jdecay": "0.2989",
- 		"Fmean": "0.6046",
- 		"Frecall": "0.6795",
- 		"Fdecay": "0.3105"
+ 		"J_F": "0.6027",
+ 		"Jmean": "0.5665",
+ 		"Jrecall": "0.6380",
+ 		"Jdecay": "0.2606",
+ 		"Fmean": "0.6390",
+ 		"Frecall": "0.7381",
+ 		"Fdecay": "0.2696"
+ 	},
+	"onavos": {
+ 		"J_F": "0.6538",
+ 		"Jmean": "0.6164",
+ 		"Jrecall": "0.6737",
+ 		"Jdecay": "0.2787",
+ 		"Fmean": "0.6913",
+ 		"Frecall": "0.7544",
+ 		"Fdecay": "0.2661"
  	}
-}
+};
