2c82c94a1ad605fb5b3f6789cecbea803a4cb1d9
scaelles <scaelles@gmail.com>
2018-05-31 10:32:33 +0200

    Final challenge 2018 results

---
+++ json_data/global_eval_test-challenge_2018.js
@@ -0,0 +1,184 @@
+var global_eval_test_challenge_2017 = {
+    "GhostDancer": {
+        "Fdecay": "0.261",
+        "Fmean": "0.328",
+        "Frecall": "0.315",
+        "J_F": "0.285",
+        "Jdecay": "0.238",
+        "Jmean": "0.242",
+        "Jrecall": "0.25"
+    },
+    "Jono": {
+        "Fdecay": "0.208",
+        "Fmean": "0.784",
+        "Frecall": "0.867",
+        "J_F": "0.747",
+        "Jdecay": "0.19",
+        "Jmean": "0.71",
+        "Jrecall": "0.795"
+    },
+    "TeamHuber99": {
+        "Fdecay": "0.295",
+        "Fmean": "0.572",
+        "Frecall": "0.625",
+        "J_F": "0.545",
+        "Jdecay": "0.257",
+        "Jmean": "0.518",
+        "Jrecall": "0.564"
+    },
+    "TeamILC_RIL": {
+        "Fdecay": "0.185",
+        "Fmean": "0.715",
+        "Frecall": "0.822",
+        "J_F": "0.695",
+        "Jdecay": "0.15",
+        "Jmean": "0.675",
+        "Jrecall": "0.77"
+    },
+    "TeamVia": {
+        "Fdecay": "0.281",
+        "Fmean": "0.624",
+        "Frecall": "0.717",
+        "J_F": "0.601",
+        "Jdecay": "0.272",
+        "Jmean": "0.577",
+        "Jrecall": "0.649"
+    },
+    "Teamsnowflower": {
+        "Fdecay": "0.221",
+        "Fmean": "0.308",
+        "Frecall": "0.272",
+        "J_F": "0.277",
+        "Jdecay": "0.191",
+        "Jmean": "0.247",
+        "Jrecall": "0.221"
+    },
+    "UIT": {
+        "Fdecay": "0.135",
+        "Fmean": "0.686",
+        "Frecall": "0.807",
+        "J_F": "0.663",
+        "Jdecay": "0.117",
+        "Jmean": "0.641",
+        "Jrecall": "0.75"
+    },
+    "Zang0": {
+        "Fdecay": "0.256",
+        "Fmean": "0.542",
+        "Frecall": "0.62",
+        "J_F": "0.532",
+        "Jdecay": "0.223",
+        "Jmean": "0.521",
+        "Jrecall": "0.601"
+    },
+    "ZilongHuang": {
+        "Fdecay": "0.246",
+        "Fmean": "0.551",
+        "Frecall": "0.608",
+        "J_F": "0.529",
+        "Jdecay": "0.2",
+        "Jmean": "0.508",
+        "Jrecall": "0.571"
+    },
+    "alextheengineer": {
+        "Fdecay": "0.297",
+        "Fmean": "0.629",
+        "Frecall": "0.71",
+        "J_F": "0.606",
+        "Jdecay": "0.262",
+        "Jmean": "0.584",
+        "Jrecall": "0.656"
+    },
+    "apata": {
+        "Fdecay": "0.302",
+        "Fmean": "0.706",
+        "Frecall": "0.798",
+        "J_F": "0.678",
+        "Jdecay": "0.277",
+        "Jmean": "0.651",
+        "Jrecall": "0.725"
+    },
+    "avishek15": {
+        "Fdecay": "0.355",
+        "Fmean": "0.481",
+        "Frecall": "0.508",
+        "J_F": "0.464",
+        "Jdecay": "0.284",
+        "Jmean": "0.448",
+        "Jrecall": "0.487"
+    },
+    "cocacolo": {
+        "Fdecay": "0.365",
+        "Fmean": "0.487",
+        "Frecall": "0.522",
+        "J_F": "0.454",
+        "Jdecay": "0.335",
+        "Jmean": "0.422",
+        "Jrecall": "0.433"
+    },
+    "dawnsix": {
+        "Fdecay": "0.259",
+        "Fmean": "0.725",
+        "Frecall": "0.803",
+        "J_F": "0.697",
+        "Jdecay": "0.231",
+        "Jmean": "0.669",
+        "Jrecall": "0.741"
+    },
+    "going": {
+        "Fdecay": "0.297",
+        "Fmean": "0.529",
+        "Frecall": "0.6",
+        "J_F": "0.514",
+        "Jdecay": "0.257",
+        "Jmean": "0.498",
+        "Jrecall": "0.561"
+    },
+    "kthac": {
+        "Fdecay": "0.331",
+        "Fmean": "0.611",
+        "Frecall": "0.676",
+        "J_F": "0.589",
+        "Jdecay": "0.307",
+        "Jmean": "0.567",
+        "Jrecall": "0.631"
+    },
+    "lixx": {
+        "Fdecay": "0.203",
+        "Fmean": "0.758",
+        "Frecall": "0.83",
+        "J_F": "0.738",
+        "Jdecay": "0.198",
+        "Jmean": "0.719",
+        "Jrecall": "0.794"
+    },
+    "techniques": [
+        "Jono",
+        "lixx",
+        "dawnsix",
+        "TeamILC_RIL",
+        "apata",
+        "UIT",
+        "alextheengineer",
+        "TeamVia",
+        "kthac",
+        "TeamHuber99",
+        "Zang0",
+        "ZilongHuang",
+        "going",
+        "yelantf",
+        "avishek15",
+        "cocacolo",
+        "GhostDancer",
+        "Teamsnowflower"
+    ],
+    "yelantf": {
+        "Fdecay": "0.339",
+        "Fmean": "0.516",
+        "Frecall": "0.571",
+        "J_F": "0.496",
+        "Jdecay": "0.274",
+        "Jmean": "0.476",
+        "Jrecall": "0.53"
+    }
+};
