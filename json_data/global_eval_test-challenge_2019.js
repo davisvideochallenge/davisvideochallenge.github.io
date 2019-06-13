0a806b9442870928ba0d97c12889424c283556ab
scaelles <scaelles@gmail.com>
2019-06-13 16:29:29 +0200

    Added final leaderboard

---
+++ json_data/global_eval_test-challenge_2019.js
@@ -0,0 +1,219 @@
+var tech_props_semisup = {
+    "Finn_zhang": {
+        "type": "over",
+        "display_name": "Finn_zhang",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "Jono": {
+        "type": "over",
+        "display_name": "Jono",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "tmtriet": {
+        "type": "over",
+        "display_name": "tmtriet",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "swoh": {
+        "type": "over",
+        "display_name": "swoh",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "theodoruszq": {
+        "type": "over",
+        "display_name": "theodoruszq",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "pandayf_jjj": {
+        "type": "over",
+        "display_name": "pandayf_jjj",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "andr345": {
+        "type": "over",
+        "display_name": "andr345",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "BytedanceCV": {
+        "type": "over",
+        "display_name": "BytedanceCV",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    }
+};
+
+var tech_props_unsup = {
+    "idilesenzulfikar": {
+        "type": "over",
+        "display_name": "idilesenzulfikar",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "JayY": {
+        "type": "over",
+        "display_name": "JayY",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "don_don": {
+        "type": "over",
+        "display_name": "don_don",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    },
+    "davis_try": {
+        "type": "over",
+        "display_name": "davis_try",
+        "sets": [
+            "test-challenge-2019"
+        ]
+    }
+};
+
+var global_eval_test_challenge_2019_semisupervised = {
+    "Finn_zhang": {
+        "J_F": 0.7665450363724275,
+        "Jmean": 0.7271047700558044,
+        "Jrecall": 0.8153470220593645,
+        "Jdecay": 0.194950293059508,
+        "Fmean": 0.8059853026890508,
+        "Frecall": 0.8726087049095084,
+        "Fdecay": 0.21985174545174768
+    },
+    "Jono": {
+        "J_F": 0.761648748248426,
+        "Jmean": 0.7291578139943524,
+        "Jrecall": 0.8174566565074927,
+        "Jdecay": 0.16323427537572813,
+        "Fmean": 0.7941396825024996,
+        "Frecall": 0.8671402360123004,
+        "Fdecay": 0.19504516942266528
+    },
+    "tmtriet": {
+        "J_F": 0.7544226750829526,
+        "Jmean": 0.7243620315558408,
+        "Jrecall": 0.8174095790456164,
+        "Jdecay": 0.1103749543709476,
+        "Fmean": 0.7844833186100644,
+        "Frecall": 0.8760253055366555,
+        "Fdecay": 0.12892656700777924
+    },
+    "swoh": {
+        "J_F": 0.7515156671046261,
+        "Jmean": 0.726455392999761,
+        "Jrecall": 0.8103777155830616,
+        "Jdecay": 0.21225295638163208,
+        "Fmean": 0.7765759412094914,
+        "Frecall": 0.8490566012737927,
+        "Fdecay": 0.24485249662483125
+    },
+    "theodoruszq": {
+        "J_F": 0.7308594858666205,
+        "Jmean": 0.7011555985754658,
+        "Jrecall": 0.7731515666458644,
+        "Jdecay": 0.2481672469859325,
+        "Fmean": 0.7605633731577752,
+        "Frecall": 0.8395196680726755,
+        "Fdecay": 0.2826771599543399
+    },
+    "pandayf_jjj": {
+        "J_F": 0.7133787173239348,
+        "Jmean": 0.6770054595336131,
+        "Jrecall": 0.7484645094358066,
+        "Jdecay": 0.2472008871877752,
+        "Fmean": 0.7497519751142564,
+        "Frecall": 0.8123013457710939,
+        "Fdecay": 0.2751821127097896
+    },
+    "andr345": {
+        "J_F": 0.7064501940328944,
+        "Jmean": 0.6850959693113161,
+        "Jrecall": 0.7810883542934804,
+        "Jdecay": 0.20310730963697757,
+        "Fmean": 0.7278044187544729,
+        "Frecall": 0.8417204838541721,
+        "Fdecay": 0.24006319452019179
+    },
+    "BytedanceCV": {
+        "J_F": 0.6915434052630987,
+        "Jmean": 0.6598435806444026,
+        "Jrecall": 0.7342046718070042,
+        "Jdecay": 0.28531225664931353,
+        "Fmean": 0.7232432298817946,
+        "Frecall": 0.8040778148703475,
+        "Fdecay": 0.31101121291323713
+    },
+    "techniques": [
+        "Finn_zhang",
+        "Jono",
+        "tmtriet",
+        "swoh",
+        "theodoruszq",
+        "pandayf_jjj",
+        "andr345",
+        "BytedanceCV"
+    ]
+};
+
+var global_eval_test_challenge_2019_unsupervised = {
+    "idilesenzulfikar": {
+        "J_F": 0.5640017114002451,
+        "Jmean": 0.5341110270260393,
+        "Jrecall": 0.6089185450633845,
+        "Jdecay": 0.01460507025057434,
+        "Fmean": 0.5938923957744511,
+        "Frecall": 0.640831473139796,
+        "Fdecay": 0.05758828577492585
+    },
+    "JayY": {
+        "J_F": 0.5624661867139968,
+        "Jmean": 0.5346001014430295,
+        "Jrecall": 0.6125688516854605,
+        "Jdecay": -0.020791086879294707,
+        "Fmean": 0.5903322719849643,
+        "Frecall": 0.6317045820657321,
+        "Fdecay": 0.0006211714110827709
+    },
+    "don_don": {
+        "J_F": 0.5158893325235174,
+        "Jmean": 0.4870946742319889,
+        "Jrecall": 0.5509096236630072,
+        "Jdecay": 0.039936879795741745,
+        "Fmean": 0.5446839908150459,
+        "Frecall": 0.5936301701888942,
+        "Fdecay": 0.07719280402315924
+    },
+    "davis_try": {
+        "J_F": 0.5042337101010432,
+        "Jmean": 0.4751807561577418,
+        "Jrecall": 0.5419017764044112,
+        "Jdecay": 0.03171619827470681,
+        "Fmean": 0.5332866640443445,
+        "Frecall": 0.5688754037904643,
+        "Fdecay": 0.054564379197652994
+    },
+    "techniques": [
+        "idilesenzulfikar",
+        "JayY",
+        "don_don",
+        "davis_try"
+    ]
+};
