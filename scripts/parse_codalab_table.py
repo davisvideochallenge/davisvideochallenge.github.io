ed2c22c0354a1ea5b72f49fc4ccc8087e80d81ff
scaelles <scaelles@gmail.com>
2020-05-12 16:11:46 +0200

    Aggregate participants results by teams in challenge tables.

--- scripts/parse_codalab_table.py
+++ scripts/parse_codalab_table.py
@@ -25,32 +25,49 @@ def clean_directory(path):
             print("Error: %s : %s" % (f, e.strerror))
 
 
+def download_table_from_url(url, table_id=0):
+    row_list = []
+    cmd = f'wget --cookies=on --keep-session-cookies --load-cookies={cookies_file} -O {output_path}/website.txt "{url}"'
+    os.system(cmd)
+    with open(f'{output_path}/website.txt', 'r') as f:
+        website = f.read()
+    clean_directory(output_path)
+    if website == '':
+        return row_list
+    soup = BeautifulSoup(website, 'lxml')
+    pages_table = soup.find_all('table')[table_id]
+    for i, row in enumerate(pages_table.find_all('tr')):
+        if i == 0:
+            continue
+        column_list = []
+        for j, column in enumerate(row.find_all('td')):
+            if j == 4:
+                break
+            column_list.append(column.get_text().replace('\n', '').replace(' ', ''))
+        row_list.append(column_list)
+    return row_list
+
+
+def get_participants_team():
+    codalab_url = f'https://competitions.codalab.org/my/competition/{competition_id}/participants'
+    row_list = download_table_from_url(codalab_url, 1)
+    participant_teams = {}
+    for participant in row_list:
+        participant_teams[participant[1]] = participant[3]
+
+    return participant_teams
+
+
 def get_table():
     table = []
     page_num = 1
     while True:
         codalab_url = f'https://competitions.codalab.org/my/competition/{competition_id}/submissions/?page={page_num}&phase={phase_id}'
-        cmd = f'wget --cookies=on --keep-session-cookies --load-cookies={cookies_file} -O {output_path}/website.txt "{codalab_url}"'
-        os.system(cmd)
-        with open(f'{output_path}/website.txt', 'r') as f:
-            website = f.read()
-        if website == '':
+        row_list = download_table_from_url(codalab_url)
+        if len(row_list) == 0:
             break
-        soup = BeautifulSoup(website, 'lxml')
-        pages_table = soup.find_all('table')[0]
-        row_list = []
-        for i, row in enumerate(pages_table.find_all('tr')):
-            if i == 0:
-                continue
-            column_list = []
-            for j, column in enumerate(row.find_all('td')):
-                if j == 4:
-                    break
-                column_list.append(column.get_text())
-            row_list.append(column_list)
         page_num += 1
         table.extend(row_list)
-        clean_directory(output_path)
     return table
 
 
@@ -59,6 +76,16 @@ def get_participants_submissions(table):
     for row in table:
         participants[row[2]].append(row[3])
     return participants
+
+
+def aggregate_participants_submissions(participants_submissions, participants_teams):
+    teams_submissions = collections.defaultdict(list)
+    for participant, submissions in participants_submissions.items():
+        if participants_teams[participant] != '':
+            teams_submissions[participants_teams[participant]].extend(submissions)
+        else:
+            teams_submissions[participant].extend(submissions)
+    return teams_submissions
 
 
 def get_participant_best_result(download_ids):
@@ -121,14 +148,17 @@ def save_json(usernames, results):
 
 if __name__ == '__main__':
     clean_directory(output_path)
+    participants_teams = get_participants_team()
+
     submissions_table = get_table()
     if len(submissions_table) == 0:
         print('No table data found!')
         exit(0)
 
     participants_submissions = get_participants_submissions(submissions_table)
+    teams_submissions = aggregate_participants_submissions(participants_submissions, participants_teams)
     participants, metrics = [], []
-    for participant, submission_ids in participants_submissions.items():
+    for participant, submission_ids in teams_submissions.items():
         best_metric = get_participant_best_result(submission_ids)
         if len(best_metric) > 0:
             participants.append(participant)
